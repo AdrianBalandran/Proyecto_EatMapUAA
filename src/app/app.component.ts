b46207bc8e4a0cf1ca67bba90ff1444954ff844d
AdrianBalandran <adrianbala76@gmail.com>
2024-11-11 22:08:13 -0600

    Pequeños añadidos
    Pequeños.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PantallacargaComponent } from "./pantallacarga/pantallacarga.component";
+import { PantallainicioComponent } from "./pantallainicio/pantallainicio.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [PantallacargaComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'Proyecto_EatMapUAA';
---
+++ src/app/pantallacarga/pantallacarga.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pantallacarga',
+  standalone: true,
+  imports: [],
+  templateUrl: './pantallacarga.component.html',
+  styleUrl: './pantallacarga.component.css'
+})
+export class PantallacargaComponent {
+
+}
