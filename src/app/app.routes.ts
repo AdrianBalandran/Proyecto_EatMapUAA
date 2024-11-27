c8cc80002c247f149532cb76b353fe0a9617be7e
AdrianBalandran <adrianbala76@gmail.com>
2024-11-27 06:50:43 -0600

    Las rutas ya funcionan
    Solucionado.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,12 @@
 import { Routes } from '@angular/router';
+import { PantallainicioComponent } from './pantallainicio/pantallainicio.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const appRoutes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige la raíz a /home
+  { path: 'home', component: PantallainicioComponent }, // Ruta /home
+  { path: 'login', component: LoginComponent },         // Ruta /login
+  { path: '**', redirectTo: '/home' }                  // Ruta para páginas no encontradas
+];
+
 
--- src/app/pantallainicio/pantallainicio.component.ts
+++ src/app/pantallainicio/pantallainicio.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { MenunavComponent } from "../menunav/menunav.component";
 import { FooterbajoComponent } from "../footerbajo/footerbajo.component";
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pantallainicio',
   standalone: true,
-  imports: [MenunavComponent, FooterbajoComponent],
+  imports: [MenunavComponent, FooterbajoComponent,RouterLink],
   templateUrl: './pantallainicio.component.html',
   styleUrl: './pantallainicio.component.css'
 })
 export class PantallainicioComponent {
 
+  constructor(private router : Router){}
 }
