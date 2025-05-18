b48c9cf40d8babc1a6f7565e20dae451a04e2085
AdrianBalandran <adrianbala76@gmail.com>
2025-05-18 11:12:04 -0600

    Solución de bug de las rutas

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,12 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, RouterModule, withHashLocation } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withHashLocation()), RouterModule, CommonModule, provideClientHydration(), HttpClient, provideHttpClient(), FormsModule, ReactiveFormsModule]
+  providers: [provideRouter(appRoutes), RouterModule, CommonModule, provideClientHydration(), HttpClient, provideHttpClient(withFetch()), FormsModule, ReactiveFormsModule]
 };
