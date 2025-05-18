12750d8436208b068dbb64e1f2e3cdc8a78b338b
AdrianBalandran <adrianbala76@gmail.com>
2025-05-18 10:50:49 -0600

    Arreglando un bug de localstorage

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter, RouterModule } from '@angular/router';
+import { provideRouter, RouterModule, withHashLocation } from '@angular/router';
 
 import { appRoutes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -8,5 +8,5 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes), RouterModule, CommonModule, provideClientHydration(), HttpClient, provideHttpClient(), FormsModule, ReactiveFormsModule]
+  providers: [provideRouter(appRoutes, withHashLocation()), RouterModule, CommonModule, provideClientHydration(), HttpClient, provideHttpClient(), FormsModule, ReactiveFormsModule]
 };
